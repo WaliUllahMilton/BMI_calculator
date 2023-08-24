b6d40883f67c94a4fb1d8a349fe8b1806b44c902
waliUllahMilton <waliullahmiltoncse@gmail.com>
2023-08-25 02:21:51 +0600

    update

--- js/script.js
+++ js/script.js
@@ -13,27 +13,44 @@ btn.addEventListener("click", () => {
     height = height.value * 12;
     height = height ** 2;
     bmi = (weight / height) * 703;
-    if (bmi < 18.5 && bmi > 15.9) {
+    if (bmi < 16 && bmi > 0) {
         result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
-        chart.style.color = "red";
-        chart.innerHTML = "You are Underweight.";
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        chart.style.color = "#953A3A";
+        chart.innerHTML = "You are Severe Thinness.";
+    } else if (bmi <= 16 && bmi > 17) {
+        result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
+        chart.style.color = "#EA487F";
+        chart.innerHTML = "You are Moderate Thinness.";
+    } else if (bmi >= 17 && bmi <= 18.5) {
+        result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
+        chart.style.color = "#EA487F";
+        chart.innerHTML = "You are Mild Thinness";
+    } else if (bmi >= 18.5 && bmi <= 25) {
         result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
         chart.style.color = "green";
         chart.innerHTML = "You are Normal";
-    } else if (bmi >= 25 && bmi <= 29.9) {
+    } else if (bmi >= 25 && bmi <= 30) {
         result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
         chart.style.color = "yellow";
         chart.innerHTML = "You are Overweight";
-    } else if (w == "" || h == "") {
-        error.innerHTML = "please insert the value"
+    } else if (bmi >= 30 && bmi <= 35) {
+        result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
+        chart.style.color = "#FF010E";
+        chart.innerHTML = "You are Obese Class I";
+    } else if (bmi >= 35 && bmi <= 40) {
+        result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
+        chart.style.color = "#A90009";
+        chart.innerHTML = "You are Obese Class II";
+    } else if (bmi > 40) {
+        result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
+        chart.style.color = "red";
+        chart.innerHTML = "You are Obese Class III";
+
+    } else if (w == "" || h == "" || bmi < 0) {
+        error.innerHTML = "please insert the value";
 
     } else {
-        error.innerHTML = "invalid information"
+        error.innerHTML = "invalid information";
     }
+    abr(1);
 })
-
-
-// // bmi = 703 * (weight.value * 2.20462 / (height * 0.0833333) ** 2);
-// bmi = 50 / 1.524 ** 2
-// console.log(bmi)
