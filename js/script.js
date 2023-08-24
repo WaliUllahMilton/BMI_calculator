317efc17369448c19d1175168a10b49d78c60788
waliUllahMilton <waliullahmiltoncse@gmail.com>
2023-08-25 00:29:20 +0600

    error updated

--- js/script.js
+++ js/script.js
@@ -3,26 +3,33 @@ let height = document.querySelector(".height");
 let btn = document.querySelector(".btn");
 let result = document.querySelector(".result");
 let chart = document.querySelector(".chart");
+let error = document.querySelector("p");
 let bmi;
 
 btn.addEventListener("click", () => {
+    let w = weight.value;
+    let h = height.value;
     weight = weight.value * 2.20462;
     height = height.value * 12;
     height = height ** 2;
     bmi = (weight / height) * 703;
-    result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
     if (bmi < 18.5 && bmi > 15.9) {
+        result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
         chart.style.color = "red";
         chart.innerHTML = "You are Underweight.";
     } else if (bmi >= 18.5 && bmi <= 24.9) {
+        result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
         chart.style.color = "green";
         chart.innerHTML = "You are Normal";
     } else if (bmi >= 25 && bmi <= 29.9) {
+        result.innerHTML = `BMI = ${bmi.toFixed(1)}`;
         chart.style.color = "yellow";
         chart.innerHTML = "You are Overweight";
+    } else if (w == "" || h == "") {
+        error.innerHTML = "please insert the value"
+
     } else {
-        chart.style.color = "blue";
-        chart.innerHTML = "You are obesity";
+        error.innerHTML = "invalid information"
     }
 })
 
